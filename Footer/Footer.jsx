e8c298f7d925ed9f92ee93e260df109eb3e52116
Ben Tom Abey <127616692+EDBOZY@users.noreply.github.com>
2023-12-14 12:59:59 +0400

    Add files via upload

---
+++ Footer/Footer.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Footer.css'
+
+function Footer() {
+  return (
+    <div className='footer'>
+        
+        <div className='text'>
+        <div className='img'>
+          <img className="download" alt="Download" src="https://c.animaapp.com/BOPxYY7d/img/download-1@2x.png" />
+        </div>
+            <span>Home</span>
+            <span>Volunteer</span>
+            <span>Donate</span>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
